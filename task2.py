da8911efca10231f8e9a7ddb0c076fb2f37a47c4
yehiafarouk24 <yehiafarouk944@gmail.com>
2026-03-18 00:43:11 +0200

    blah blah

--- task2.py
+++ task2.py
@@ -1,52 +1,41 @@
 import numpy as np
-import pandas as pd
-class neuron:
-    def __init__(self,y , error, net):
-        self.y = y
-        self.error = error
-        self.net = net
 
-class mlp:
-    def __init__(self, layers, hidden_n, eta, epochs, bias, actv_fn):
-        self.layers = layers
-        self.hidden_n = hidden_n
-        self.eta = eta
-        self.epochs = epochs
+class Layer:
+    def __init__(self, input_num, output_num, bias):
+        self.weights = np.random.randn(input_num, output_num) 
         self.bias = bias
-        self.actv_fn = actv_fn
+        if self.bias == 0:
+            self.biases = np.zeros((1, output_num))
+        else:
+            self.biases = np.random.randn(1, output_num)
 
-    def initialization(self, X_train, y):
-            outputs = len(y.value_counts())
-            inputs = len(X_train.columns)
-            print(outputs, inputs) 
-            biaslist = []
-            neuronBig = []
-            for i in range(self.layers):
-                neuronList = []
-                if i == self.layers:
-                     for j in outputs:
-                          neuronList.append(neuron(0,0,0))
-                else:
-                     for j in range(self.hidden_n):
-                          neuronList.append(neuron(0,0,0))
-                neuronBig.append(neuronList)
+class NeuralNetwork:
+    def __init__(self, hidden_layers, hidden_neurons, learning_rate, epochs, bias, activation_function, input_size = 5, output_size = 3):
+        self.layers = []
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.hidden_layers = hidden_layers
+        self.hidden_neurons = hidden_neurons
+        self.bias = bias
+        self.activation_function = activation_function
 
-            for i in range(self.layers):
-                weightsbig = []
-                if i == 0:
-                    weightslist = []
-                    weightslist.append(np.random.rand(self.hidden_n,inputs))
-                elif i == self.layers:
-                     weightslist.append(np.random.rand(outputs,self.hidden_n))
-                else:
-                     weightslist.append(np.random.rand(self.hidden_n,self.hidden_n))
-                weightsbig.append(weightslist)
-
-
-df = pd.read_csv("penguins.csv")
-x_train = df.iloc[:,1:]
-print(x_train)
-y = df["Species"]
-obj = mlp(2,3,1,1,1,1)
-obj.initialization(x_train,y)
-            
+        for i in range(hidden_layers + 1):
+            if i == 0:
+                lyr =Layer(input_size, hidden_neurons, bias)
+                self.layers.append(lyr)
+                print(f'Layer : {i}')
+                print(lyr.weights)
+                print(lyr.biases)
+            elif i == hidden_layers:
+                lyr =Layer(hidden_neurons, output_size, bias)
+                print('Output Layer')
+                print(lyr.weights)
+                print(lyr.biases)
+                self.layers.append(lyr)
+            else:
+                print(f'Layer : {i}')
+                lyr =Layer(hidden_neurons, hidden_neurons, bias)
+                print(lyr.weights)
+                print(lyr.biases)
+                self.layers.append(lyr)
+obj = NeuralNetwork(hidden_layers=2, hidden_neurons=4, learning_rate=0.0341, epochs=104345, bias=True, activation_function='blahblah')
